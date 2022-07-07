a8b7a826649deba5deb1792149d82f5b4a2e3962
kyn02242 <70257475+kyn02242@users.noreply.github.com>
2022-07-07 14:03:06 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string str;
+    while (1) {
+        getline(cin, str);
+        if (str == "")
+            break;
+        cout << str << endl;
+    }
+    return 0;
+}
