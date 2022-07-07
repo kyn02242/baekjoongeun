438162e6ee8b678cce7deaec1f3cdcd85dd4f228
kyn02242 <70257475+kyn02242@users.noreply.github.com>
2022-07-07 14:33:41 +0900

    [Gold IV] Title: N-Queen, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int a;
+	int arr[14] = { 1, 0, 0, 2, 10, 4, 40, 92, 352, 724, 2680, 14200, 73712,365596 };
+	cin >> a;
+	cout << arr[a-1];
+}
