dad22f8a74e7850d9c42655d6c6f69193a8c39d5
kyn02242 <70257475+kyn02242@users.noreply.github.com>
2022-07-18 15:50:17 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	
+	int n;
+	cin >> n;
+	if (n % 2 == 0) {
+		cout << "CY";
+	}
+	else {
+		cout << "SK";
+	}
+}
