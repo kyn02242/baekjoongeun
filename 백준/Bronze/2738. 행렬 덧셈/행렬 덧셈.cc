73262715debc5a8644ee5d99c0fd1ae80b1cc0aa
kyn02242 <70257475+kyn02242@users.noreply.github.com>
2022-07-21 14:34:34 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 0 ms, Memory: 2152 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.cc
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+#define endl '\n'
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n,m;
+    cin>>n>>m;
+    vector<vector<int>> a(n,vector<int>(m));
+    vector<vector<int>> b(n,vector<int>(m));
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>a[i][j];
+        }
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>b[i][j];
+            a[i][j]+=b[i][j];
+        }
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cout<<a[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
