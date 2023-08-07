a58a962d4cab11a60b51484028a5759855fb16ea
kyn02242 <70257475+kyn02242@users.noreply.github.com>
2023-08-07 15:32:28 +0900

    [Bronze V] Title: 몇개고?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27294. 몇개고？/몇개고？.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <queue>
+#include <stdio.h>
+#include <sstream>
+#include <memory>
+#include <deque>
+#include <set>
+
+using namespace std;
+
+
+int main()
+{
+    int t,n;
+    cin>>t>>n;
+    if(t>=12&&t<=16){
+        if(n==0){
+            cout<<320<<endl;
+            return 0;
+        }
+    }
+    cout<<280<<endl;
+    return 0;
+}
