824f4ad7262dc15e687116dd235c229d00a0c820
kyn02242 <70257475+kyn02242@users.noreply.github.com>
2023-08-07 15:26:31 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <queue>
+#include <stdio.h>
+#include <sstream>
+#include <memory>
+#include <deque>
+#include <set>
+
+using namespace std;
+
+
+int main()
+{
+    int n;
+    cin>>n;
+    if(n==0){
+        cout<<"YONSEI"<<endl;
+    }
+    else{
+        cout<<"Leading the Way to the Future"<<endl;
+    }
+
+    return 0;
+}
