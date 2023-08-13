6cd0aab9806c01ab5451bdbe3a3d5bb073514460
kyn02242 <70257475+kyn02242@users.noreply.github.com>
2023-08-13 18:18:32 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <queue>
+#include <stdio.h>
+#include <sstream>
+#include <memory>
+#include <deque>
+#include <set>
+#define endl '\n'
+#define MAX -987654321
+
+using namespace std;
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(NULL);
+	cin.tie(NULL);
+
+	string first = "       _.-;;-._";
+	string second = "'-..-'|   ||   |";
+	string third = "'-..-'|_.-;;-._|";
+	string fourth = "'-..-'|   ||   |";
+	string fifth = "'-..-'|_.-''-._|";
+
+	cout<<first<<endl<<second<<endl<<third<<endl<<fourth<<endl<<fifth<<endl;
+
+	return 0;
+}
