18a40020eaf3f2568c232dc085b44a251b5d719a
kyn02242 <70257475+kyn02242@users.noreply.github.com>
2023-08-13 18:28:34 +0900

    [Bronze V] Title: Арифметическая магия, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18096. Арифметическая магия/Арифметическая магия.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <queue>
+#include <stdio.h>
+#include <sstream>
+#include <memory>
+#include <deque>
+#include <set>
+#define endl '\n'
+
+using namespace std;
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(NULL);
+	cin.tie(NULL);
+
+	int x;
+	cin>>x;
+	cout<<1;
+
+	return 0;
+}
