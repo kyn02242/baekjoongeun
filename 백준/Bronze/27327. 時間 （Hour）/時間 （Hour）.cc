61a6ee58f58593fd01a7eef6d47978294c8c1503
kyn02242 <70257475+kyn02242@users.noreply.github.com>
2023-08-13 18:24:14 +0900

    [Bronze V] Title: 時間 (Hour), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27327. 時間 （Hour）/時間 （Hour）.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <queue>
+#include <stdio.h>
+#include <sstream>
+#include <memory>
+#include <deque>
+#include <set>
+#define endl '\n'
+#define MAX -987654321
+
+using namespace std;
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(NULL);
+	cin.tie(NULL);
+
+	int x;
+	cin>>x;
+	cout<<x*24<<endl;
+
+	return 0;
+}
