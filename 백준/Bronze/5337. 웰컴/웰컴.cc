1a062c80d8ac975fd9ef98ba8aaccd1734d3c718
kyn02242 <70257475+kyn02242@users.noreply.github.com>
2023-08-13 18:21:00 +0900

    [Bronze V] Title: 웰컴, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <queue>
+#include <stdio.h>
+#include <sstream>
+#include <memory>
+#include <deque>
+#include <set>
+#define endl '\n'
+#define MAX -987654321
+
+using namespace std;
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(NULL);
+	cin.tie(NULL);
+
+	string first = ".  .   .";
+	string second = "|  | _ | _. _ ._ _  _";
+	string third = "|/\\|(/.|(_.(_)[ | )(/.";
+
+	cout<<first<<endl<<second<<endl<<third<<endl;
+
+	return 0;
+}
