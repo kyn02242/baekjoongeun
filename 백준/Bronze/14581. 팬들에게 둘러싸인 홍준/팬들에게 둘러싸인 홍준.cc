6a05a85e7f929a42d35187d22f284e0a9adae933
kyn02242 <70257475+kyn02242@users.noreply.github.com>
2023-08-14 22:46:26 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <queue>
+#include <stdio.h>
+#include <sstream>
+#include <memory>
+#include <deque>
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    string str;
+
+    cin >> str;
+    cout << ":fan::fan::fan:" << endl;
+    cout<<":fan::"<<str<<"::fan:"<<endl;
+    cout << ":fan::fan::fan:" << endl;
+
+    
+    return 0;
+}
