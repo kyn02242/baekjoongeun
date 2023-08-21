03f6489d841588d25b259837de5117114347561e
kyn02242 <70257475+kyn02242@users.noreply.github.com>
2023-08-21 11:53:06 +0900

    [unrated] Title: 뒤에서 5등 위로, Time: 0.01 ms, Memory: 3.67 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181852. 뒤에서 5등 위로/뒤에서 5등 위로.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> num_list) {
+    sort(num_list.begin(),num_list.end());
+    vector<int> answer(num_list.begin()+5,num_list.end());
+    
+    
+    
+    
+    return answer;
+}
