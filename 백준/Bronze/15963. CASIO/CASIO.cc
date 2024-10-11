87264b1a0ca5a815b65ec56f82465eb243f83875
kyn02242 <70257475+kyn02242@users.noreply.github.com>
2024-10-12 06:09:44 +0900

    [Bronze V] Title: CASIO, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15963. CASIO/CASIO.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <queue>
+#include <stdio.h>
+
+using namespace std;
+
+int main(){
+    long long a, b;
+    cin >> a >> b;
+    if (a == b){
+        cout << 1;
+    }
+    else{
+        cout << 0;
+    }
+}
