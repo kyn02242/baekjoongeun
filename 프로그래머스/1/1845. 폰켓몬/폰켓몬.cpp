9d9a993e18f98d6e1b1360d0de365eb0879b1340
kyn02242 <70257475+kyn02242@users.noreply.github.com>
2024-10-23 04:39:52 +0900

    [level 1] Title: 폰켓몬, Time: 1.37 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
@@ -1,33 +1,13 @@
 #include <vector>
-#include <algorithm>
+#include <set>
+
 using namespace std;
+
 
 int solution(vector<int> nums)
 {
     int answer = 0;
-    int n = nums.size();
-    int flag = 1;
-    vector<int> v;
-
-    sort(nums.begin(), nums.end());
-
-    v.push_back(nums[0]);
-    for (auto i : nums)
-    {
-        if (v.back() != i)
-        {
-            v.push_back(i);
-        }
-    }
-
-    if (v.size() >= n / 2)
-    {
-        answer = n / 2;
-    }
-    else
-    {
-        answer = v.size();
-    }
-
+    set<int> s(nums.begin(), nums.end());
+    answer = min(nums.size()/2, s.size());
     return answer;
 }
