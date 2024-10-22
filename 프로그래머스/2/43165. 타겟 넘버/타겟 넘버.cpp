5149c1217e424da75ad495cd4c7a256f7492cfb4
kyn02242 <70257475+kyn02242@users.noreply.github.com>
2024-10-23 03:14:53 +0900

    [level 2] Title: 타겟 넘버, Time: 70.03 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -6,34 +6,24 @@
 
 using namespace std;
 
-int solution(vector<int> numbers, int target) {
-    int answer = 0;
-    int n = numbers.size();
-    vector<int> v(pow(2,n));
-    for(int i=0;i<n;i++){
-        int flag = pow(2,n)/pow(2,i+1);
-        int cnt = 0;
-        bool oper = true;
-        for(int j=0;j<pow(2,n);j++){
-            if(oper){
-                v[j]+=numbers[i];
-                cnt++;
-            }
-            else{
-                v[j]-=numbers[i];
-                cnt++;
-            }
-            if(cnt==flag){
-                oper = !oper;
-                cnt = 0;
-            }
-        }
-    }
-    for(auto i : v){
-        if(i==target){
+int answer = 0;
+
+void DFS(vector<int> numbers, int sum, int index, int target)
+{
+    if (index == numbers.size())
+    {
+        if (target == sum)
+        {
             answer++;
         }
-        //cout<<i<<endl;
+        return;
     }
+    DFS(numbers, sum + numbers[index], index + 1, target);
+    DFS(numbers, sum - numbers[index], index + 1, target);
+}
+
+int solution(vector<int> numbers, int target)
+{
+    DFS(numbers, 0, 0, target);
     return answer;
 }
