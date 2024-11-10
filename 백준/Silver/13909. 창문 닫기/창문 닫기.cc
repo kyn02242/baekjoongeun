c258f4a750b0ae6d0ca1ef1ce3adeccd47062c10
kyn02242 <70257475+kyn02242@users.noreply.github.com>
2024-11-11 00:45:29 +0900

    [Silver V] Title: 창문 닫기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.cc
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#define endl '\n'
+
+using namespace std;
+
+int main()
+{
+    int cnt = 0;
+
+    int n;
+    cin >> n;
+    cout<< floor(sqrt(n));
+    return 0;
+}
