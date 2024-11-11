95a2b626ed7cef612301ae8c903d5db5ace8eb2b
kyn02242 <70257475+kyn02242@users.noreply.github.com>
2024-11-12 00:25:16 +0900

    [Bronze IV] Title: 녹색거탑, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.cc
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <stack>
+#include <deque>
+#define endl '\n'
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+    cout << pow(2, n);
+
+    return 0;
+}
