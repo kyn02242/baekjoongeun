82062c215e8c57258eb04a546d8482f29b796f35
kyn02242 <70257475+kyn02242@users.noreply.github.com>
2024-11-22 00:40:05 +0900

    [Bronze V] Title: Hook, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10189. Hook/Hook.cc
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <stack>
+#include <deque>
+#include <numeric>
+#define endl '\n'
+#define MAX INT_MAX
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cout << "#  # #### #### #  #"<<endl;
+    cout << "#### #  # #  # # #"<<endl;
+    cout << "#### #  # #  # # #"<<endl;
+    cout << "#  # #### #### #  #"<<endl;
+
+    return 0;
+}
