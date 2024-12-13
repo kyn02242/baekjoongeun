7c20ee0d7cb77791d5587a5c2aff5bb620f71977
kyn02242 <70257475+kyn02242@users.noreply.github.com>
2024-12-13 14:49:02 +0900

    [Bronze V] Title: Plane, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8370. Plane/Plane.cc
@@ -0,0 +1,31 @@
+#include <string>
+#include <string.h>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <stack>
+#include <deque>
+#include <numeric>
+#include <regex>
+#define endl '\n'
+#define ll long long int
+
+using namespace std;
+
+int main()
+{
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    int a, b, c, d;
+
+    cin >> a >> b >> c >> d;
+    cout << a * b + c * d;
+    return 0;
+}
